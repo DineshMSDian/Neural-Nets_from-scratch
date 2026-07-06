f689c9da7cf743199d573926b1046485ab782b72
DineshMSDian <dinesh2742004@gmail.com>
2026-07-06 17:42:49 +0530

    NN-from_scratch: MicroGrad: Added the ability to store the parents information of a object
    Signed-off-by: DineshMSDian <dinesh2742004@gmail.com>

--- micrograd.py
+++ micrograd.py
@@ -2,18 +2,19 @@
 
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, children=()):
         self.data = data
+        self._prev = set(children) # Previous nodes that created me.
 
     def __repr__(self):
         return f'Value(data={self.data})'
     
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other)) # Value(data, _children) This new Value was created from these two parent nodes (self = a and other = b).
         return out
     
     def __mul__(self, other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other)) # Value(data, _children) This new Value was created from these two parent nodes (self = a and other = b).)
         return out
     
 a = Value(2.0)
@@ -21,5 +22,5 @@ b = Value(-3.0)
 c = Value(10.0)
 
 d = a*b + c
-
 print(d)
+print(c._prev)
