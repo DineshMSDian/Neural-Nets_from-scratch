3446a0b4ffed24ad879ddabb9c330ba19cf6d93c
DineshMSDian <dinesh2742004@gmail.com>
2026-07-07 18:57:50 +0530

    NN-from_scratch: MicroGrad: initialized the gradient valuse to '0.0' initialy, to perfrom backprop, and added label for each object that acces the 'Value' class
    Signed-off-by: DineshMSDian <dinesh2742004@gmail.com>

--- micrograd.py
+++ micrograd.py
@@ -2,9 +2,12 @@
 
 class Value:
 
-    def __init__(self, data, children=(), _op=''):
+    def __init__(self, data, children=(), _op='', label = ''):
         self.data = data
+        self.grad = 0.0
         self._prev = set(children) # Previous nodes that created me.
+        self._op = _op
+        self.lebel = label
 
     def __repr__(self):
         return f'Value(data={self.data})'
@@ -17,10 +20,10 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*') # Value(data, _children) This new Value was created from these two parent nodes (self = a and other = b).)
         return out
     
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
 
-d = a*b + c
+d = a*b + c; d.label='d'
 print(d)
 print(c._prev)
